38f01316e4cb42385f96512b935b5d8aea609f54
Rhythm Garg <rhythmgarg05@gmail.com>
2024-02-27 17:35:34 +0530

    dfnldjfj

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,32 @@ import * as THREE from "three";
 import "./index.css";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import gsap from "gsap";
-import {GLTFLoader} from "three/addons/loaders/GLTFLoader";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
+import { FBXLoader } from "three/addons/loaders/FBXLoader";
 
 const scene = new THREE.Scene();
 
-const loader = new GLTFLoader();
+const fbxLoader = new FBXLoader();
+fbxLoader.load(
+  "/HutForUnity.fbx",
+  (object) => {
+    console.log(object);
+    object.scale.set(0.1, 0.1, 0.1);
+    scene.add(object);
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  (error) => {
+    console.log(error);
+  }
+);
 
-loader.load("src/assets/Models/dae_diorama_-_rustborn/scene.gltf", function (gltf) {
-  scene.add(gltf.scene);
-}, undefined, function (error) {
-  console.error(error);
-});
-
-const light = new THREE.PointLight(0xffffff, 1000, 100);
-scene.add(light);
-light.position.set(15, 15, 15);
+const spotlight = new THREE.SpotLight(0xffffff, 1000, 100);
+scene.add(spotlight);
+spotlight.position.set(500, 500, 500);
+spotlight.rotation.set(90, 90, 90);
+scene.background = new THREE.Color(0x111111);
 
 const camera = new THREE.PerspectiveCamera(
   45,
@@ -25,7 +36,7 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 // camera.position.x = 0;
-camera.position.z = 35;
+camera.position.z = 500;
 // camera.position.y = 10;
 // camera.rotation.x = 180;
 // camera.rotation.z = 90;
@@ -41,8 +52,8 @@ renderer.render(scene, camera);
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-controls.enablePan = false;
-controls.enableZoom = false;
+// controls.enablePan = false;
+// controls.enableZoom = false;
 // controls.autoRotate = true;
 controls.autoRotateSpeed = 5;
 
@@ -62,13 +73,23 @@ loop();
 
 const tl = gsap.timeline({
   defaults: {
-    duration: 1
+    duration: 1,
   },
 });
 
-tl.fromTo(scene.scale, {x:0, y: 0, z:0 }, {x: 1, y: 1, z: 1 });
-tl.fromTo("nav", {y: "-100%"}, {y: "0%", duration: 2, ease: "power2.out"}, "-=1");
-tl.fromTo(".title", {opacity: 0, y: 50}, {opacity: 1, y: 0, duration: 1, ease: "power2.out"}, "-=1");
+// tl.fromTo(object.scale, {x:0, y: 0, z:0 }, {x: 1, y: 1, z: 1 });
+tl.fromTo(
+  "nav",
+  { y: "-100%" },
+  { y: "0%", duration: 2, ease: "power2.out" },
+  "-=1"
+);
+tl.fromTo(
+  ".title",
+  { opacity: 0, y: 50 },
+  { opacity: 1, y: 0, duration: 1, ease: "power2.out" },
+  "-=1"
+);
 
 let mouseDown = false;
 let rgb = [12, 23, 34];
